2f4ac2106b5815e948ee05c0a1bc5ef9f1ef329c
cem <tekmencem@gmail.com>
2021-07-05 20:27:46 +0300

    spring dersi

---
+++ src/main/java/com/sha/springbootmicroservice1product/SpringBootMicroservice1ProductApplication.java
@@ -0,0 +1,14 @@
+package com.sha.springbootmicroservice1product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+public class SpringBootMicroservice1ProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroservice1ProductApplication.class, args);
+	}
+
+}
